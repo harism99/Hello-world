bb39d08b73047f4bb0d5a1800c4004184aac27a1
Github <mabdullaharis@gmail.com>
2019-03-30 10:25:22 +0530

    Modified

---
+++ src/ConditionalOperator.java
@@ -0,0 +1,23 @@
+public class ConditionalOperator {
+    public static void main(String args[]){
+        int a, b;
+        a=30;
+        b=20;
+        switch (a){
+            case 30: {
+                System.out.println("A is 30");
+            }
+                break;
+            case 20:{
+                System.out.println("A is 20");
+            }
+            break;
+            case 10:{
+                System.out.println("A is 10");
+            }
+            break;
+
+        }
+
+    }
+}
--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
-    public static void main(String[] args){
-        System.out.println("Hello Everyone");
+    public static void main(String args[]){
+        System.out.println("Is everything fine");
+        System.out.print("Hello Everyone");
     }
 }
